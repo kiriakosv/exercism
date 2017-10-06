b80deeb9ccb74b13342cf9cebd4bc78b05c2fa77
Kiriakos Velissariou <kir.velissariou@gmail.com>
2017-10-06 21:00:08 +0300

    Add solution for difference of squares problem

---
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,21 @@
+class Squares
+  def initialize(number)
+    @number = number
+  end
+
+  def square_of_sum
+    (0..@number).inject(:+) ** 2
+  end
+
+  def sum_of_squares
+    (0..@number).inject { |sum, n| sum + n ** 2 }
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+end
+
+module BookKeeping
+  VERSION = 4
+end

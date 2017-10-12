fba230d5f6debc82442d40c7cce0d0fcd17d72e7
Kiriakos Velissariou <kir.velissariou@gmail.com>
2017-10-12 21:32:14 +0300

    Add solution for pangram problem

---
+++ ruby/pangram/pangram.rb
@@ -0,0 +1,12 @@
+module Pangram
+  ALPHABET_LENGTH = 26
+  
+  def self.pangram?(sentence)
+    letters = sentence.downcase.split("").select { |l| l =~ /[a-z]/ }
+    letters.uniq.size == ALPHABET_LENGTH
+  end
+end
+
+module BookKeeping
+  VERSION = 5
+end
